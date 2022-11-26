b46263380a30cf988da0dfc80c8548740e1ee1a3
nobushi95 <nobushi1121@gmail.com>
2022-11-27 02:00:41 +0900

    sqliteバージョン出力確認

--- sqlite/csharp/sqlite-study/sqlite-study/Program.cs
+++ sqlite/csharp/sqlite-study/sqlite-study/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace sqlite_study
+﻿using System.Data.SQLite;
+
+namespace sqlite_study
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var sqlConnectionSb = new SQLiteConnectionStringBuilder { DataSource = ":memory:" };
+
+            using (var connect = new SQLiteConnection(sqlConnectionSb.ToString()))
+            using (var cmd = new SQLiteCommand(connect))
+            {
+                connect.Open();
+                cmd.CommandText = "select sqlite_version()";
+                Console.WriteLine($"sqlite_version: {cmd.ExecuteScalar()}");
+            }
         }
     }
 }
